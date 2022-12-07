2af112452d28c9df5e1c41edc998fa395c3a2c32
s21010 <s21010@std.it-college.ac.jp>
2022-12-07 12:07:55 +0900

    test

--- app/src/main/java/jp/ac/it_college/std/s21010/fishapplication/MainActivity.kt
+++ app/src/main/java/jp/ac/it_college/std/s21010/fishapplication/MainActivity.kt
@@ -1,11 +1,20 @@
 package jp.ac.it_college.std.s21010.fishapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import jp.ac.it_college.std.s21010.fishapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        title = "魚図鑑"
+        binding.pictorialbtn.setOnClickListener {
+            val intent = Intent(this, pictorialbookActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
